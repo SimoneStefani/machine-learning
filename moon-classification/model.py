6010219eb534e310280992a3243785c002398ae1
Simone Stefani <s.stefani95@gmail.com>
2017-08-13 17:04:25 +0200

    Implement logistic regression

--- moon-classification/model.py
+++ moon-classification/model.py
@@ -5,7 +5,7 @@ import sklearn
 import sklearn.datasets
 import sklearn.linear_model
 import matplotlib
-
+from utilities import plot_decision_boundary
 
 ##---------------------------------------------------------
 # GENERATE DATASET
@@ -19,4 +19,20 @@ np.random.seed(0)
 X, y = sklearn.datasets.make_moons(200, noise = 0.20)
 
 # Generate scatter plot of the dataset
-plt.scatter(X[:, 0], X[:, 1], s = 40, c = y, cmap = plt.cm.Spectral)
+plt.scatter(X[:, 0], X[:, 1], s = 10, c = y, cmap = plt.cm.Spectral)
+#plt.show()
+
+
+##---------------------------------------------------------
+# LOGISTIC REGRESSION
+##---------------------------------------------------------
+
+# Train a logistic regression model to fit the dataset
+log_reg_clf = sklearn.linear_model.LogisticRegressionCV()
+log_reg_clf.fit(X, y)
+
+# Plot the dataset with the classification boundary found
+# through logistic regression
+plot_decision_boundary(lambda x: log_reg_clf.predict(x), X, y)
+plt.title("Logistic Regression")
+#plt.show()
