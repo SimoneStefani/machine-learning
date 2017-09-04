8ffc9f7944af54bc966e69eb6105be0a9e11ace6
Simone Stefani <s.stefani95@gmail.com>
2017-09-04 21:02:32 +0200

    Add He initialization

--- data-manipulation/initialization.py
+++ data-manipulation/initialization.py
@@ -17,6 +17,8 @@ plt.rcParams['image.cmap'] = 'gray'
 
 # Load image dataset
 train_X, train_Y, test_X, test_Y = load_dataset()
+plt.clf()
+plt.close()
 
 
 ##---------------------------------------------------------
@@ -122,3 +124,73 @@ def initialize_parameters_random(layers_dims):
         parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
 
     return parameters
+
+
+##---------------------------------------------------------
+# HE INITIALIZATION
+##---------------------------------------------------------
+
+# Arguments:  layer_dims -- python array (list) containing the size of each layer.
+# Returns:    parameters -- python dictionary containing parameters "W1", "b1", ..., "WL", "bL":
+#               W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+#               b1 -- bias vector of shape (layers_dims[1], 1)
+#               ...
+#               WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+#               bL -- bias vector of shape (layers_dims[L], 1)
+def initialize_parameters_he(layers_dims):
+    
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1 # Integer representing the number of layers
+     
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(2./layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        
+    return parameters
+
+
+##---------------------------------------------------------
+# RUN MODEL
+##---------------------------------------------------------
+
+# Zero initialization
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 1.5])
+axes.set_ylim([-1.5, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+# Random initialization
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 1.5])
+axes.set_ylim([-1.5, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+# He initialization
+parameters = model(train_X, train_Y, initialization = "he")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with He initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 1.5])
+axes.set_ylim([-1.5, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
